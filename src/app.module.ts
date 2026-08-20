ba7f6304a0d6e17ef989be63d58403b56b9326f1
Mariam Yasser Mymoon <mariam.me421@gmail.com>
2026-08-20 07:10:53 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,33 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule as configModule, ConfigService } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
-      logging: true,
+    configModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        DB_HOST: joi.string().required(),
+        DB_PORT: joi.number().required(),
+        DB_USERNAME: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_DATABASE: joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        logging: true,
+      }),
     }),
     MoviesModule,
   ],
